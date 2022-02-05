fd024c70c1649dbae5dd723cb3cf8d88c9357ef9
Joshua Doucet <joshua.doucet@live.com>
2022-02-05 16:48:51 -0700

    initial push. Angular setup, and component generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductListItemComponent } from './components/product-list-item/product-list-item.component';
+import { ProductCartItemComponent } from './components/product-cart-item/product-cart-item.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartProductsComponent } from './components/cart-products/cart-products.component';
+import { CartCheckoutComponent } from './components/cart-checkout/cart-checkout.component';
+import { OrderConfirmComponent } from './components/order-confirm/order-confirm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductListItemComponent,
+    ProductCartItemComponent,
+    CartComponent,
+    ProductDetailsComponent,
+    CartProductsComponent,
+    CartCheckoutComponent,
+    OrderConfirmComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/product-cart-item/product-cart-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-cart-item',
+  templateUrl: './product-cart-item.component.html',
+  styleUrls: ['./product-cart-item.component.css']
+})
+export class ProductCartItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
