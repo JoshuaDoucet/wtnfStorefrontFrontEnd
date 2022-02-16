54334c53af0753a216b86b4ee9fc8c918ed15c86
Joshua Doucet <joshua.doucet@live.com>
2022-02-16 14:47:09 -0700

    began checkout form component

--- src/app/components/cart-checkout/cart-checkout.component.ts
+++ src/app/components/cart-checkout/cart-checkout.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-checkout.component.css']
 })
 export class CartCheckoutComponent implements OnInit {
+  firstName: string = 'First Name';
+  lastName: string = 'Last Name';
+  phone: number = 8885550000;
+  email: string = 'your@email.com';
+  addr1: string = 'Line 1 Address';
+  addr2: string = 'Line 2 Address';
+  city: string = 'City';
+  state: string = 'State';
+  zip: number = -1;
+  country: string = 'Country';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  placeOrder(): void {
+    console.log("Order placed")
+  }
 }
