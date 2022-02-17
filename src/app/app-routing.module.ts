ee8e1ea86c94e4d036ac194e40e0dbb0abd38965
Joshua Doucet <joshua.doucet@live.com>
2022-02-17 10:13:02 -0700

    order confirm page and order service started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { SigninComponent } from './components/signin/signin.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { CartComponent } from './components/cart/cart.component';
+import { OrderConfirmComponent } from './components/order-confirm/order-confirm.component';
 
 const routes: Routes = [
   {path: "", component: ProductsComponent},
   {path: "cart", component: CartComponent},
+  {path: "cart/confirm", component: OrderConfirmComponent},
   {path: "products", component: ProductsComponent},
   {path: "products/:id", component: ProductDetailsComponent},
   {path: "signin", component: SigninComponent},
--- src/app/components/cart-checkout/cart-checkout.component.ts
+++ src/app/components/cart-checkout/cart-checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { User } from 'src/app/models/user';
+import { UserProfileService } from 'src/app/services/user-profile/user-profile.service';
 
 @Component({
   selector: 'app-cart-checkout',
@@ -16,13 +19,36 @@ export class CartCheckoutComponent implements OnInit {
   state: string = '';
   zip: number | undefined;
   country: string = '';
+  user: User = new User();
 
-  constructor() { }
+  constructor(private userProfileService: UserProfileService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.autoFillCheckoutForm()
+  }
+
+  autoFillCheckoutForm(){
+    const userId = localStorage.getItem('userId')
+    if(userId){
+      this.userProfileService.getUser(userId).subscribe({
+        error: err => {
+          alert("Cannot verify user credentials. Sign-In")
+        },
+        next: user => {
+          this.firstName = user.first_name;
+          this.lastName = user.last_name;
+          this.phone = user.phone;
+          this.email = user.email;
+        }
+      })
+      }
   }
 
   placeOrder(): void {
-    console.log("Order placed")
+    let route = `/cart/confirm`;
+    this.router.navigate([route], {
+      state: { /* Pass info to confirm page here */}
+    });
   }
 }
